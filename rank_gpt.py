53187adf82f5eb84f9945613ca430b948cbbec09
Weiwei Sun <sunnweiwei@gmail.com>
2023-04-20 13:50:54 +0800

    code update

---
+++ rank_gpt.py
@@ -0,0 +1,278 @@
+import copy
+from tqdm import tqdm
+import time
+import openai
+import json
+import tiktoken
+
+
+class SafeOpenai:
+    def __init__(self, keys=None, start_id=None, proxy=None):
+        if isinstance(keys, str):
+            keys = [keys]
+        if keys is None:
+            raise "Please provide OpenAI Key."
+
+        self.key = keys
+        self.key_id = start_id or 0
+        self.key_id = self.key_id % len(self.key)
+        openai.proxy = proxy
+        openai.api_key = self.key[self.key_id % len(self.key)]
+
+    def chat(self, *args, return_text=False, reduce_length=False, **kwargs):
+        while True:
+            try:
+                completion = openai.ChatCompletion.create(*args, **kwargs, timeout=30)
+                break
+            except Exception as e:
+                print(str(e))
+                if "This model's maximum context length is" in str(e):
+                    print('reduce_length')
+                    return 'ERROR::reduce_length'
+                self.key_id = (self.key_id + 1) % len(self.key)
+                openai.api_key = self.key[self.key_id]
+                time.sleep(0.1)
+        if return_text:
+            completion = completion['choices'][0]['message']['content']
+        return completion
+
+    def text(self, *args, return_text=False, reduce_length=False, **kwargs):
+        while True:
+            try:
+                completion = openai.Completion.create(*args, **kwargs)
+                break
+            except Exception as e:
+                print(e)
+                if "This model's maximum context length is" in str(e):
+                    print('reduce_length')
+                    return 'ERROR::reduce_length'
+                self.key_id = (self.key_id + 1) % len(self.key)
+                openai.api_key = self.key[self.key_id]
+                time.sleep(0.5)
+        if return_text:
+            completion = completion['choices'][0]['text']
+        return completion
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.get_encoding(model)
+    except:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        return num_tokens_from_messages(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        tokens_per_message, tokens_per_name = 0, 0
+
+    num_tokens = 0
+    if isinstance(messages, list):
+        for message in messages:
+            num_tokens += tokens_per_message
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":
+                    num_tokens += tokens_per_name
+    else:
+        num_tokens += len(encoding.encode(messages))
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+def max_tokens(model):
+    if model == 'gpt-4':
+        return 8192
+    else:
+        return 4096
+
+
+def run_retriever(topics, searcher, qrels=None, k=100, qid=None):
+    ranks = []
+    if isinstance(topics, str):
+        hits = searcher.search(topics, k=k)
+        ranks.append({'query': topics, 'hits': []})
+        rank = 0
+        for hit in hits:
+            rank += 1
+            content = json.loads(searcher.doc(hit.docid).raw())
+            if 'title' in content:
+                content = 'Title: ' + content['title'] + ' ' + 'Content: ' + content['text']
+            else:
+                content = content['contents']
+            content = ' '.join(content.split())
+            ranks[-1]['hits'].append({
+                'content': content,
+                'qid': qid, 'docid': hit.docid, 'rank': rank, 'score': hit.score})
+        return ranks[-1]
+
+    for qid in tqdm(topics):
+        if qid in qrels:
+            query = topics[qid]['title']
+            ranks.append({'query': query, 'hits': []})
+            hits = searcher.search(query, k=k)
+            rank = 0
+            for hit in hits:
+                rank += 1
+                content = json.loads(searcher.doc(hit.docid).raw())
+                if 'title' in content:
+                    content = 'Title: ' + content['title'] + ' ' + 'Content: ' + content['text']
+                else:
+                    content = content['contents']
+                content = ' '.join(content.split())
+                ranks[-1]['hits'].append({
+                    'content': content,
+                    'qid': qid, 'docid': hit.docid, 'rank': rank, 'score': hit.score})
+    return ranks
+
+
+def get_prefix_prompt(query, num):
+    return [{'role': 'system',
+             'content': "You are RankGPT, an intelligent assistant that can rank passages based on their relevancy to the query."},
+            {'role': 'user',
+             'content': f"I will provide you with {num} passages, each indicated by number identifier []. \nRank the passages based on their relevance to query: {query}."},
+            {'role': 'assistant', 'content': 'Okay, please provide the passages.'}]
+
+
+def get_post_prompt(query, num):
+    return f"Search Query: {query}. \nRank the {num} passages above based on their relevance to the search query. The passages should be listed in descending order using identifiers. The most relevant passages should be listed first. The output format should be [] > [], e.g., [20] > [19]. Only response the ranking results, do not say any word or explain."
+
+
+def create_permutation_instruction(item=None, rank_start=0, rank_end=100, model_name='gpt-3.5-turbo'):
+    query = item['query']
+    num = len(item['hits'][rank_start: rank_end])
+
+    max_length = 300
+    while True:
+        messages = get_prefix_prompt(query, num)
+        rank = 0
+        for hit in item['hits'][rank_start: rank_end]:
+            rank += 1
+            content = hit['content']
+            content = content.replace('Title: Content: ', '')
+            content = content.strip()
+
+            content = ' '.join(content.split()[:int(max_length)])
+            messages.append({'role': 'user', 'content': f"[{rank}] {content}"})
+            messages.append({'role': 'assistant', 'content': f'Received passage [{rank}].'})
+        messages.append({'role': 'user', 'content': get_post_prompt(query, num)})
+
+        if num_tokens_from_messages(messages, model_name) <= max_tokens(model_name) - 200:
+            break
+        else:
+            max_length -= 1
+    return messages
+
+
+def run_llm(messages, openai_key=None, model_name="gpt-3.5-turbo"):
+    agent = SafeOpenai(openai_key)
+    response = agent.chat(model=model_name, messages=messages, temperature=0, return_text=True)
+    print(response)
+    return response
+
+
+def clean_response(response: str):
+    new_response = ''
+    for c in response:
+        if not c.isdigit():
+            new_response += ' '
+        else:
+            new_response += c
+    new_response = new_response.strip()
+    return new_response
+
+
+def remove_duplicate(response):
+    new_response = []
+    for c in response:
+        if c not in new_response:
+            new_response.append(c)
+    return new_response
+
+
+def receive_permutation(item, permutation, rank_start=0, rank_end=100):
+    response = clean_response(permutation)
+    response = [int(x) - 1 for x in response.split()]
+    response = remove_duplicate(response)
+    cut_range = copy.deepcopy(item['hits'][rank_start: rank_end])
+    original_rank = [tt for tt in range(len(cut_range))]
+    response = [ss for ss in response if ss in original_rank]
+    response = response + [tt for tt in original_rank if tt not in response]
+    for j, x in enumerate(response):
+        item['hits'][j + rank_start] = {
+            'content': cut_range[x]['content'], 'qid': cut_range[x]['qid'], 'docid': cut_range[x]['docid'],
+            'rank': cut_range[j]['rank'], 'score': cut_range[j]['score']}
+    return item
+
+
+def permutation_pipeline(item=None, rank_start=0, rank_end=100, model_name='gpt-3.5-turbo', openai_key=None):
+    messages = create_permutation_instruction(item=item, rank_start=rank_start, rank_end=rank_end,
+                                              model_name='gpt-3.5-turbo')
+    permutation = run_llm(messages, openai_key=openai_key, model_name=model_name)
+    item = receive_permutation(item, permutation, rank_start=rank_start, rank_end=rank_end)
+    return item
+
+
+def sliding_windows(item=None, rank_start=0, rank_end=100, window_size=20, step=10, model_name='gpt-3.5-turbo',
+                    openai_key=None):
+    item = copy.deepcopy(item)
+    end_pos = rank_start
+    start_pos = rank_end - window_size
+    while start_pos >= rank_start:
+        start_pos = max(start_pos, cut_start)
+        print(f'## Rank {start_pos} - {end_pos}')
+        item = permutation_pipeline(item, start_pos, end_pos, model_name=model_name, openai_key=openai_key)
+
+        end_pos = end_pos - step
+        start_pos = start_pos - step
+    self.write_file(f"{file}-final", rank=cur_rank)
+    print(f"File write on {file}-final")
+    if return_rank:
+        return cur_rank
+
+
+def write_eval_file(rank_results, file):
+    with open(file, 'w') as f:
+        for i in range(len(rank_results)):
+            rank = 1
+            hits = rank_results[i]['hits']
+            for hit in hits:
+                f.write(f"{hit['qid']} Q0 {hit['docid']} {rank} {hit['score']} rank\n")
+                rank += 1
+    return True
+
+
+def main():
+    from pyserini.search import LuceneSearcher
+    from pyserini.search import get_topics, get_qrels
+    import tempfile
+
+    openai_key = None  # Your openai key
+
+    searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
+    topics = get_topics('dl19-passage')
+    qrels = get_qrels('dl19-passage')
+
+    rank_results = run_retriever(topics, searcher, qrels, k=100)
+
+    new_results = []
+    for item in tqdm(rank_results):
+        new_item = permutation_pipeline(item, rank_start=0, rank_end=20, model_name='gpt-3.5-turbo', openai_key=openai_key)
+        new_results.append(new_item)
+
+    temp_file = tempfile.NamedTemporaryFile(delete=False).name
+    write_eval_file(new_results, temp_file)
+    from trec_eval import EvalFunction
+
+    EvalFunction.eval(['-c', '-m', 'ndcg_cut.10', 'dl19-passage', temp_file])
+
+
+if __name__ == '__main__':
+    main()
---
+++ trec_eval.py
@@ -0,0 +1,116 @@
+import os
+import re
+import subprocess
+import sys
+import platform
+import pandas as pd
+import tempfile
+
+from pyserini.search import get_qrels_file
+from pyserini.util import download_evaluation_script
+
+
+class EvalFunction:
+    @staticmethod
+    def trunc(qrels, run):
+        qrels = get_qrels_file(qrels)
+        run = pd.read_csv(run, delim_whitespace=True, header=None)
+        qrels = pd.read_csv(qrels, delim_whitespace=True, header=None)
+        run[0] = run[0].astype(str)
+        qrels[0] = qrels[0].astype(str)
+
+        qrels = qrels[qrels[0].isin(run[0])]
+        temp_file = tempfile.NamedTemporaryFile(delete=False).name
+        qrels.to_csv(temp_file, sep='\t', header=None, index=None)
+        return temp_file
+
+    @staticmethod
+    def eval(args, trunc=True):
+        script_path = download_evaluation_script('trec_eval')
+        cmd_prefix = ['java', '-jar', script_path]
+        # args = sys.argv
+
+        # Option to discard non-judged hits in run file
+        judged_docs_only = ''
+        judged_result = []
+        cutoffs = []
+
+        if '-remove-unjudged' in args:
+            judged_docs_only = args.pop(args.index('-remove-unjudged'))
+
+        if any([i.startswith('judged.') for i in args]):
+            # Find what position the arg is in.
+            idx = [i.startswith('judged.') for i in args].index(True)
+            cutoffs = args.pop(idx)
+            cutoffs = list(map(int, cutoffs[7:].split(',')))
+            # Get rid of the '-m' before the 'judged.xxx' option
+            args.pop(idx - 1)
+
+        temp_file = ''
+
+        if len(args) > 1:
+            if trunc:
+                args[-2] = EvalFunction.trunc(args[-2], args[-1])
+                print('Trunc', args[-2])
+
+            if not os.path.exists(args[-2]):
+                args[-2] = get_qrels_file(args[-2])
+            if os.path.exists(args[-1]):
+                # Convert run to trec if it's on msmarco
+                with open(args[-1]) as f:
+                    first_line = f.readline()
+                if 'Q0' not in first_line:
+                    temp_file = tempfile.NamedTemporaryFile(delete=False).name
+                    print('msmarco run detected. Converting to trec...')
+                    run = pd.read_csv(args[-1], delim_whitespace=True, header=None,
+                                      names=['query_id', 'doc_id', 'rank'])
+                    run['score'] = 1 / run['rank']
+                    run.insert(1, 'Q0', 'Q0')
+                    run['name'] = 'TEMPRUN'
+                    run.to_csv(temp_file, sep='\t', header=None, index=None)
+                    args[-1] = temp_file
+
+            run = pd.read_csv(args[-1], delim_whitespace=True, header=None)
+            qrels = pd.read_csv(args[-2], delim_whitespace=True, header=None)
+
+            # cast doc_id column as string
+            run[0] = run[0].astype(str)
+            qrels[0] = qrels[0].astype(str)
+
+            # Discard non-judged hits
+
+            if judged_docs_only:
+                if not temp_file:
+                    temp_file = tempfile.NamedTemporaryFile(delete=False).name
+                judged_indexes = pd.merge(run[[0, 2]].reset_index(), qrels[[0, 2]], on=[0, 2])['index']
+                run = run.loc[judged_indexes]
+                run.to_csv(temp_file, sep='\t', header=None, index=None)
+                args[-1] = temp_file
+            # Measure judged@cutoffs
+            for cutoff in cutoffs:
+                run_cutoff = run.groupby(0).head(cutoff)
+                judged = len(pd.merge(run_cutoff[[0, 2]], qrels[[0, 2]], on=[0, 2])) / len(run_cutoff)
+                metric_name = f'judged_{cutoff}'
+                judged_result.append(f'{metric_name:22}\tall\t{judged:.4f}')
+            cmd = cmd_prefix + args[1:]
+        else:
+            cmd = cmd_prefix
+
+        print(f'Running command: {cmd}')
+        shell = platform.system() == "Windows"
+        process = subprocess.Popen(cmd,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   shell=shell)
+        stdout, stderr = process.communicate()
+        if stderr:
+            print(stderr.decode("utf-8"))
+
+        print('Results:')
+        print(stdout.decode("utf-8").rstrip())
+
+        for judged in judged_result:
+            print(judged)
+
+        if temp_file:
+            os.remove(temp_file)
